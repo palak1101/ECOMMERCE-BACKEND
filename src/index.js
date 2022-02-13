d3b4e751283b999cf3a7760c0505206560e8e42a
Palak <palaktiwari156@gmail.com>
2022-02-14 00:31:48 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use("/api/v1/product", productRoutes)
 
 
 app.get('/', (req, res) => {
-    res.send(`Server running at PORT ${port}`)
+    res.send(`Server running at PORT ${port} (Deployed from workflow)`)
 })
 
 app.listen(port, (req, res) => {
